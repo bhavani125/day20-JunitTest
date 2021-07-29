e997c2e730d40f68e281569ed71d80793eb51a71
bhavani125 <girinenibhavani12@gmil.com>
2021-07-29 14:17:40 +0530

    Added:Code of validating LastName

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -5,10 +5,18 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     String namePattern = "^[A-Z][a-z]{3,}$";
+
     //Creating validateFirstName Method
     public boolean validateFirstName(String name) {
         Pattern pattern = Pattern.compile(namePattern);
         Matcher matcher = pattern.matcher(name);
         return matcher.find();
+    }
+    //Creating validateLastName
+    public boolean validateLastName(String name) {
+        Pattern pattern = Pattern.compile(namePattern);
+        Matcher matcher = pattern.matcher(name);
+        return matcher.find();
+
     }
 }
--- src/test/java/com/bridgelabz/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationTest.java
@@ -6,22 +6,38 @@ import org.junit.Test;
 public class UserRegistrationTest {
     //Creating Object for UserRegistration class
     UserRegistration userRegistration = new UserRegistration();
+    //testCases for validating  firstName
     @Test
     public void givenFirstName_whenValid_thenReturnTrue() {
         //calling validateFirstName method
         boolean result = userRegistration.validateFirstName("Bhavani");
         Assert.assertTrue(result);
     }
-
     @Test
     public void givenFirstName_WhenStartingNotCapital_ShouldReturnFalse() {
         boolean result = userRegistration.validateFirstName("bhavani");
         Assert.assertFalse(result);
     }
-
     @Test
     public void givenFirstName_WhenLessThanThreeChars_ShouldReturnFalse() {
         boolean result = userRegistration.validateFirstName("Bh");
+        Assert.assertFalse(result);
+    }
+   //testCases for validating  lastName
+    @Test
+    public void givenLastName_whenValid_thenReturnTrue() {
+        //calling validateFirstName method
+        boolean result = userRegistration.validateLastName("Girineni");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenLastName_WhenStartingNotCapital_ShouldReturnFalse() {
+        boolean result = userRegistration.validateLastName("girineni");
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void givenLastName_WhenLessThanThreeChars_ShouldReturnFalse() {
+        boolean result = userRegistration.validateLastName("Gi");
         Assert.assertFalse(result);
     }
 }
